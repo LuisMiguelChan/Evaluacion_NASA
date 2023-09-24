e6761eca09201c1957978cbbac0f33a6cffc5210
Luis Miguel Chan <lmchan@nasa.com.mx>
2023-09-24 16:40:19 -0500

    Cambios en Graficos

--- Evaluacion_NASAWinForms/Forms/xENC160010.Designer.cs
+++ Evaluacion_NASAWinForms/Forms/xENC160010.Designer.cs
@@ -29,22 +29,42 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(xENC160010));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
             this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.RibbonControl)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.LargeImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SmallImageStatus)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.MainApplicationMenu)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
             this.SuspendLayout();
             // 
             // RibbonControl
@@ -58,7 +78,7 @@
             this.RibbonControl.SearchEditItem.EditWidth = 150;
             this.RibbonControl.SearchEditItem.Id = -5000;
             this.RibbonControl.SearchEditItem.ImageOptions.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.True;
-            this.RibbonControl.Size = new System.Drawing.Size(800, 49);
+            this.RibbonControl.Size = new System.Drawing.Size(787, 49);
             this.RibbonControl.Toolbar.ShowCustomizeItem = false;
             // 
             // MainDefaultLookAndFeel
@@ -103,52 +123,144 @@
             // 
             // XtraConsoftToolBar1
             // 
-            this.XtraConsoftToolBar1.Location = new System.Drawing.Point(753, 49);
+            this.XtraConsoftToolBar1.Location = new System.Drawing.Point(740, 49);
+            this.XtraConsoftToolBar1.Size = new System.Drawing.Size(47, 375);
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Controls.Add(this.chart3);
+            this.layoutControl1.Controls.Add(this.chart2);
+            this.layoutControl1.Controls.Add(this.textBox2);
+            this.layoutControl1.Controls.Add(this.textBox1);
             this.layoutControl1.Controls.Add(this.chart1);
             this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.layoutControl1.Location = new System.Drawing.Point(0, 49);
             this.layoutControl1.Name = "layoutControl1";
             this.layoutControl1.Root = this.Root;
-            this.layoutControl1.Size = new System.Drawing.Size(753, 370);
+            this.layoutControl1.Size = new System.Drawing.Size(740, 375);
             this.layoutControl1.TabIndex = 7;
             this.layoutControl1.Text = "layoutControl1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(445, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(283, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 36);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(356, 161);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
             // 
             // Root
             // 
             this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
             this.Root.GroupBordersVisible = false;
             this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem1});
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4,
+            this.layoutControlItem5});
             this.Root.Name = "Root";
-            this.Root.Size = new System.Drawing.Size(753, 370);
+            this.Root.Size = new System.Drawing.Size(740, 375);
             this.Root.TextVisible = false;
-            // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(109, 12);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(632, 346);
-            this.chart1.TabIndex = 4;
-            this.chart1.Text = "chart1";
             // 
             // layoutControlItem1
             // 
             this.layoutControlItem1.Control = this.chart1;
-            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 24);
             this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(733, 350);
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(93, 13);
+            this.layoutControlItem1.Size = new System.Drawing.Size(360, 165);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.textBox1;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(344, 24);
+            this.layoutControlItem2.Text = "Total Cursos: ";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(86, 13);
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.textBox2;
+            this.layoutControlItem3.Location = new System.Drawing.Point(344, 0);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(376, 24);
+            this.layoutControlItem3.Text = "Total Categorias: ";
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(86, 13);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(372, 36);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(356, 327);
+            this.chart2.TabIndex = 7;
+            this.chart2.Text = "chart2";
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.chart2;
+            this.layoutControlItem4.Location = new System.Drawing.Point(360, 24);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(360, 331);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // chart3
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart3.Legends.Add(legend1);
+            this.chart3.Location = new System.Drawing.Point(12, 201);
+            this.chart3.Name = "chart3";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series3";
+            this.chart3.Series.Add(series1);
+            this.chart3.Size = new System.Drawing.Size(356, 162);
+            this.chart3.TabIndex = 8;
+            this.chart3.Text = "chart3";
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.chart3;
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 189);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(360, 166);
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem5.TextVisible = false;
             // 
             // xENC160010
             // 
@@ -156,7 +268,7 @@
             this.Appearance.Options.UseImage = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(787, 455);
             this.Controls.Add(this.layoutControl1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.IconOptions.Icon = ((System.Drawing.Icon)(resources.GetObject("xENC160010.IconOptions.Icon")));
@@ -173,9 +285,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.MainApplicationMenu)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,5 +305,13 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private DevExpress.XtraLayout.LayoutControlGroup Root;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
     }
 }
--- Evaluacion_NASAWinForms/Forms/xENC160010.cs
+++ Evaluacion_NASAWinForms/Forms/xENC160010.cs
@@ -20,17 +20,112 @@ namespace Evaluacion_NASAWinForms.Forms
 
         private void xENC160010_Load(object sender, EventArgs e)
         {
-            chart1.Series["Series1"].ChartType = SeriesChartType.Pie;
-            chart1.Series["Series1"].BorderWidth = 5;
-            Dictionary<string, int> dic = new Dictionary<string, int>();
-            dic.Add("Angular",20);
-            dic.Add("Consoft",15);
-            dic.Add("CONSOFT2",15);
+            Evaluacion_NASACore.BusinessLayer.CLS_DBCURSOS_BAL CursosBal = new Evaluacion_NASACore.BusinessLayer.CLS_DBCURSOS_BAL();
+            Evaluacion_NASACore.BusinessLayer.CLS_DBCATALOGOS_BAL CatalogosBal = new Evaluacion_NASACore.BusinessLayer.CLS_DBCATALOGOS_BAL();
+            Evaluacion_NASACore.BusinessLayer.CLS_REGISTRO_BAL RegistradosBal = new Evaluacion_NASACore.BusinessLayer.CLS_REGISTRO_BAL();
+            DataTable cursos = CursosBal.GetTable();
+            DataTable categorias = CatalogosBal.GetTable();
+            DataTable registrados = RegistradosBal.GetTable();
 
-            foreach (KeyValuePair<string, int> d in dic)
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            Dictionary<string, int> dic2 = new Dictionary<string, int>(); //pie
+            Dictionary<string, decimal> dic3 = new Dictionary<string, decimal>();
+            
+            int cursosEcategorias;
+            int registradosEcursos;
+            decimal importCursos = 0;
+            int numcursos = 0;
+            int numcategorias = 0;
+
+            //Chart numero 1
+            chart1.Series["Series1"].ChartType = SeriesChartType.Column;
+            chart1.Titles.Add("REGISTROS EN CURSOS");
+            
+            foreach (DataRow row in cursos.Rows)
             {
-                chart1.Series["Series1"].Points.AddXY(d.Key, d.Value);
+                registradosEcursos = 0;
+                numcursos += 1;
+                foreach (DataRow row2 in registrados.Rows)
+                {
+                    if (Convert.ToInt32(row["num_doc"]) == Convert.ToInt32(row2["id_curso"]))
+                        registradosEcursos += 1;
+                }
+                if (!dic.ContainsKey(row["nomcurso"].ToString()) && registradosEcursos > 0)
+                {
+                    dic.Add(row["nomcurso"].ToString(), registradosEcursos);
+                }
             }
+
+            foreach (var val1 in dic)
+            {
+                Series series = chart1.Series.Add(val1.Key);
+                series.Points.Add(val1.Value);
+                series.Label = (val1.Value.ToString());
+            }
+
+            textBox1.Text = numcursos.ToString();
+
+            //Chart numero 2
+            chart2.Series["Series2"].ChartType = SeriesChartType.Pie;
+            chart2.Series["Series2"].BorderWidth = 5;
+            chart2.Titles.Add("CURSOS EN CATEGORIA");
+
+            foreach (DataRow row in categorias.Rows)
+            {
+                cursosEcategorias = 0;
+                numcategorias += 1;
+                foreach (DataRow row2 in cursos.Rows)
+                {
+                    if (Convert.ToInt32(row["id_categoria"]) == Convert.ToInt32(row2["id_categoria"]))
+                        cursosEcategorias += 1;
+                }
+                if (!dic2.ContainsKey(row["nomcat"].ToString()) && cursosEcategorias > 0)
+                {
+                    dic2.Add(row["nomcat"].ToString(), cursosEcategorias);
+                }
+            }
+
+            foreach (var val2 in dic2)
+            {
+                chart2.Series["Series2"].Points.AddXY(val2.Key,val2.Value);
+            }
+
+            textBox2.Text = numcategorias.ToString();
+
+
+            //Chart numero 3
+            chart3.Series["Series3"].ChartType = SeriesChartType.Column;
+            chart3.Series["Series3"].BorderWidth = 5;
+            chart3.Titles.Add("IMPORTE POR CURSOS");
+
+            foreach (DataRow row in cursos.Rows)
+            {
+                bool ent = false;
+                foreach (DataRow row2 in registrados.Rows)
+                {
+                    if (Convert.ToInt32(row["num_doc"]) == Convert.ToInt32(row2["id_curso"]))
+                    {
+                        importCursos += Convert.ToDecimal(row2["importe"]);
+                        ent = true;
+                    } 
+                }
+                if (!dic3.ContainsKey(row["nomcurso"].ToString()) && ent == true)
+                {
+                    dic3.Add(row["nomcurso"].ToString(), importCursos);
+                }
+            }
+
+            foreach (var val3 in dic3)
+            {
+                Series series = chart3.Series.Add(val3.Key);
+                series.Points.Add(Convert.ToInt32(val3.Value));
+                series.Label = (val3.Value.ToString());
+            }
+        }
+
+        private void chart3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
